022dadfd06a4d11e0a0b7df125676e72ca9e65ce
Sofi Dobriak <sofi.dobriak@gmail.com>
2025-10-19 15:33:13 +0300

    feat: add fonts and meta info

---
+++ app/layout.tsx
@@ -0,0 +1,58 @@
+import './globals.css';
+import localFont from 'next/font/local';
+
+const figtree = localFont({
+  src: [
+    {
+      path: '../public/fonts/figtree-v9-latin-regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/figtree-v9-latin-600.woff2',
+      weight: '600',
+      style: 'normal',
+    },
+  ],
+  display: 'swap',
+  variable: '--font-figtree',
+});
+
+const clashDisplay = localFont({
+  src: [
+    {
+      path: '../public/fonts/ClashDisplay-Medium.otf',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/ClashDisplay-Semibold.otf',
+      weight: '600',
+      style: 'normal',
+    },
+  ],
+  display: 'swap',
+  variable: '--font-clashDisplay',
+});
+
+export const metadata = {
+  title: 'Tactix',
+  description: 'Promotional landing page for a marketing agency',
+  icons: {
+    icon: '/favicon.svg',
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${figtree.variable} ${clashDisplay.variable}`}>
+        {children}
+      </body>
+    </html>
+  );
+}
